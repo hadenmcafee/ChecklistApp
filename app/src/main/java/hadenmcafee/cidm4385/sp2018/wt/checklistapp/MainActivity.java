37e4e094b3945fbbf9bddf1351c211ba4067c962
Haden McAfee <hadenmcafee@gmail.com>
2018-02-23 20:06:39 -0600

    Initial commit of blank Android project.

---
+++ app/src/main/java/hadenmcafee/cidm4385/sp2018/wt/checklistapp/MainActivity.java
@@ -0,0 +1,13 @@
+package hadenmcafee.cidm4385.sp2018.wt.checklistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
